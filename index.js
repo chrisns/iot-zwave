066d2d49ba4591ad835298ec63214c8dbb0a5476
Chris Nesbitt-Smith <chris@cns.me.uk>
2018-01-14 21:18:54 +0000

    refactor

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 /* eslint-disable no-undef, camelcase, no-unused-vars, no-return-assign */
 
-const AWS = require('aws-sdk')
-const awsIot = require('aws-iot-device-sdk')
-const Queue = require('promise-queue')
-const ZWave = require('openzwave-shared')
+const AWS = require("aws-sdk")
+const Queue = require("promise-queue")
+const ZWave = require("openzwave-shared")
+const util = require("util")
 
 const _ = {
-  set: require('lodash.set'),
-  mapValues: require('lodash.mapvalues'),
-  pickBy: require('lodash.pickby'),
-  get: require('lodash.get')
+  set: require("lodash.set"),
+  mapValues: require("lodash.mapvalues"),
+  pickBy: require("lodash.pickby"),
+  get: require("lodash.get")
 }
 const {AWS_ACCESS_KEY, AWS_SECRET_ACCESS_KEY, AWS_IOT_ENDPOINT_HOST, AWS_REGION, ZWAVE_NETWORK_KEY, DEBUG, DEVICE} = process.env
 
@@ -35,13 +35,18 @@ const iotdata = new AWS.IotData({
   debug: DEBUG
 })
 
-const thingShadows = awsIot.thingShadow({
+const AWSMqtt = require("aws-mqtt-client").default
+
+const awsMqttClient = new AWSMqtt({
   accessKeyId: AWS_ACCESS_KEY,
-  secretKey: AWS_SECRET_ACCESS_KEY,
-  host: AWS_IOT_ENDPOINT_HOST,
-  protocol: 'wss',
-  debug: DEBUG
+  secretAccessKey: AWS_SECRET_ACCESS_KEY,
+  endpointAddress: AWS_IOT_ENDPOINT_HOST,
+  region: AWS_REGION,
+  logger: console
 })
+
+awsMqttClient.async_publish = util.promisify(awsMqttClient.publish)
+awsMqttClient.async_subscribe = util.promisify(awsMqttClient.subscribe)
 
 const zwave = new ZWave({
   Logging: DEBUG,
@@ -50,7 +55,7 @@ const zwave = new ZWave({
 })
 
 exports.zwave_driver_failed = () => {
-  console.log('driver failed')
+  console.log("driver failed")
   zwave.disconnect()
   process.exit()
 }
@@ -58,23 +63,28 @@ exports.zwave_driver_failed = () => {
 exports.value_update = (nodeid, comclass, value) =>
   exports.update_thing(
     value.node_id,
-    _.set({}, `${value.genre}.${value.label}`, value.value))
+    _.set({}, `${value.genre}.${value.label}`, value.value || 0))
 
 exports.update_thing = async (thing_id, update) => {
   let payload = {state: {reported: update}}
-
-  let shadow = await iotdata.getThingShadow({thingName: `zwave_${home_id}_${thing_id}`}).promise()
-
+  let shadow = {payload: "{}"}
+  try {
+    shadow = await iotdata.getThingShadow({thingName: `zwave_${home_id}_${thing_id}`}).promise()
+  }
+  catch (error) {
+    console.error(error)
+  }
   Object.entries(update).forEach(([genre, paramset]) =>
     Object.entries(paramset).forEach(([param, value]) => {
-      let path = `state.desired[${genre}][${param}]`
-      let existing = _.get(JSON.parse(shadow.payload), path)
-      if (existing && existing === value) {
-        _.set(payload, path, null)
+        let path = `state.desired[${genre}][${param}]`
+        let existing = _.get(JSON.parse(shadow.payload), path)
+        if (existing && existing === value) {
+          _.set(payload, path, null)
+        }
       }
-    }
     )
   )
+  console.log("PAYLOAD", JSON.stringify(payload))
   return queue.add(() =>
     iotdata.updateThingShadow({
       thingName: `zwave_${home_id}_${thing_id}`,
@@ -83,7 +93,7 @@ exports.update_thing = async (thing_id, update) => {
       .catch(() => // @TODO make more specfic to when there isn't a thing
         iot.createThing({
           thingName: params.thingName,
-          thingTypeName: 'zwave'
+          thingTypeName: "zwave"
         }).promise()
           .then(() => iotdata.updateThingShadow({
             thingName: `zwave_${home_id}_${thing_id}`,
@@ -93,8 +103,9 @@ exports.update_thing = async (thing_id, update) => {
   )
 }
 
-exports.setValue = (thing_id, genre, label, value) =>
-  zwave.setValue(...things[thing_id][genre][label].split('-').concat([value]))
+exports.setValue = (thing_id, genre, label, value) => {
+  zwave.setValue(...things[thing_id][genre][label].split("-").concat([value]))
+}
 
 exports.zwave_on_value_added = (nodeid, comclass, value) =>
   things = _.set(things, `zwave_${home_id}_${nodeid}.${value.genre}.${value.label}`, value.value_id)
@@ -103,9 +114,13 @@ exports.thingShadows_on_delta_thing = (thingName, stateObject) => {
   if (thingName === `zwave_${home_id}`) return
   Object.entries(stateObject.state).forEach(([genre, values]) =>
     Object.entries(values).forEach(([label, value]) =>
-      exports.silent_try(() => exports.setValue(thingName, genre, label, value))
+      exports.silent_try(() => exports.setValue(thingName, genre, label, value)) //@TODO silent catching is really not the answer
     )
   )
+  iotdata.updateThingShadow({ //@TODO well this is an awfully gross hack isn't it
+    thingName: thingName,
+    payload: JSON.stringify({state: {desired: null}})
+  }).promise()
 }
 
 exports.silent_try = func => {
@@ -117,7 +132,7 @@ exports.silent_try = func => {
 }
 
 exports.SIGINT = () => {
-  console.log('disconnecting...')
+  console.log("disconnecting...")
   zwave.disconnect(DEVICE)
   process.exit()
 }
@@ -128,18 +143,33 @@ exports.zwave_on_node_removed = nodeid =>
 exports.zwave_on_node_available = async (nodeid, nodeinfo) => {
   let params = {
     thingName: `zwave_${home_id}_${nodeid}`,
-    thingTypeName: 'zwave',
+    thingTypeName: "zwave",
     attributePayload: {
-      attributes: _.mapValues(_.pickBy(nodeinfo, info => info.length >= 1 && info.length <= 800), v => v.replace(new RegExp(' ', 'g'), '_'))
+      attributes: _.mapValues(_.pickBy(nodeinfo, info => info.length >= 1 && info.length <= 800), v => v.replace(new RegExp("[ |\(|\)]", "g"), "_"))
     }
   }
-  console.log('node available', params)
+  console.log("node available", params)
   try {
     await iot.updateThing(params).promise()
   } catch (error) {
     await iot.createThing(params).promise()
   }
-  await thingShadows.register(params.thingName)
+  subscribe_to_thing(params.thingName)
+}
+
+const subscriptions = []
+
+const subscribe_to_thing = async (thingName, topic = `$aws/things/${thingName}/shadow/update/delta`) => {
+  if (subscriptions.includes(topic)) return
+  subscriptions.push(topic)
+  try {
+    await awsMqttClient.async_subscribe(topic, {qos: 1})
+    await awsMqttClient.async_publish(`$aws/things/${thingName}/shadow/update`, JSON.stringify({state: {desired: {ignore_me: null}}}))
+
+  }
+  catch (error) {
+    console.error(error)
+  }
 }
 
 exports.zwave_on_driver_ready = homeid => {
@@ -149,7 +179,8 @@ exports.zwave_on_driver_ready = homeid => {
   }
   return iot.createThing(params).promise()
     .catch(() => iot.updateThing(params).promise())
-    .then(() => thingShadows.register(params.thingName))
+    .then(() => subscribe_to_thing(params.thingName))
+  // .then(() => thingShadows.register(params.thingName))
 }
 
 exports.thingShadow_on_delta_hub = (thingName, stateObject) => {
@@ -163,45 +194,53 @@ exports.thingShadow_on_delta_hub = (thingName, stateObject) => {
 
   if (stateObject.state.secureAddNode) {
     zwave.addNode(true)
-    update('secureAddNode')
+    update("secureAddNode")
   }
   if (stateObject.state.healNetwork) {
     zwave.healNetwork()
-    update('healNetwork')
+    update("healNetwork")
   }
   if (stateObject.state.addNode) {
     zwave.addNode()
-    update('addNode')
+    update("addNode")
   }
   if (stateObject.state.cancelControllerCommand) {
     zwave.cancelControllerCommand()
-    update('cancelControllerCommand')
+    update("cancelControllerCommand")
   }
   if (stateObject.state.removeNode) {
     zwave.removeNode()
-    update('removeNode')
+    update("removeNode")
   }
   if (stateObject.state.softReset) {
     zwave.softReset()
-    update('softReset')
+    update("softReset")
   }
 }
 
-if (!global.it) {
-  zwave.connect(DEVICE)
-  zwave.on('value added', exports.zwave_on_value_added)
-  zwave.on('value added', (nodeid, comclass, value) => console.debug('value added', nodeid, comclass, value))
-  zwave.on('driver ready', homeid => console.log('scanning homeid=0x%s...', homeid.toString(16)))
-  zwave.on('scan complete', () => console.log('====> scan complete, hit ^C to finish.'))
+// if (!global.it) {
+zwave.connect(DEVICE)
+zwave.on("value added", exports.zwave_on_value_added)
+zwave.on("value added", (nodeid, comclass, value) => console.debug("value added", nodeid, comclass, value))
+zwave.on("driver ready", homeid => console.log("scanning homeid=0x%s...", homeid.toString(16)))
+zwave.on("scan complete", () => console.log("====> scan complete."))
 
-  thingShadows.on('delta', exports.thingShadow_on_delta_hub)
-  zwave.on('driver ready', exports.zwave_on_driver_ready)
-  zwave.on('node available', exports.zwave_on_node_available)
-  zwave.on('node removed', exports.zwave_on_node_removed)
-  process.on('SIGINT', exports.SIGINT)
-  thingShadows.on('delta', exports.thingShadows_on_delta_thing)
-  zwave.on('driver failed', exports.zwave_driver_failed)
+// thingShadows.on('delta', exports.thingShadow_on_delta_hub)
+zwave.on("driver ready", exports.zwave_on_driver_ready)
+zwave.on("node available", exports.zwave_on_node_available)
+zwave.on("node removed", exports.zwave_on_node_removed)
+process.on("SIGINT", exports.SIGINT)
+// thingShadows.on('delta', exports.thingShadows_on_delta_thing)
+zwave.on("driver failed", exports.zwave_driver_failed)
 
-  zwave.on('value added', exports.value_update)
-  zwave.on('value changed', exports.value_update)
-}
+zwave.on("value added", exports.value_update)
+zwave.on("value changed", exports.value_update)
+
+awsMqttClient.on("message", (topic, message) => {
+  let thing_name = topic.split("/")[2]
+  let payload = JSON.parse(message.toString())
+  exports.thingShadow_on_delta_hub(thing_name, payload)
+  exports.thingShadows_on_delta_thing(thing_name, payload)
+})
+
+// }

2166ef42f28af819b78b8499954eb8629d7f8b84
Chris Nesbitt-Smith <chris@cns.me.uk>
2019-01-20 21:32:45 +0000

    fix scene event publish

--- index.js
+++ index.js
@@ -312,7 +312,7 @@ zwave.on('notification', (nodeid, notif, help) => logger("NOTIFICATION:", nodeid
 zwave.on('controller command', (nodeId, retVal, state, message) => logger("controller command:", nodeId, retVal, state, message))
 
 zwave.on('scene event', (nodeid, data) => logger("SCENE", nodeid, data))
-zwave.on('scene event', (nodeid, sceneid) => awsMqttClient.publish(`$aws/things/zwave_${home_id}_${nodeid}/shadow/update`, JSON.stringify({ state: { reported: { user: { sceneid: sceneid } } } })))
+zwave.on('scene event', (nodeid, sceneid) => awsMqttClient.publish(`$aws/things/zwave_${home_id}_${nodeid}/shadow/update`, JSON.stringify({ state: { reported: { user: { "Scene ID": sceneid } } } })))
 
 process.on("SIGINT", exports.SIGINT)
 zwave.on("driver failed", exports.SIGINT)
